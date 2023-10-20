17aa44c580ec5dac1f58f82a3447fe6a52f109d8
ankitchudasama1399 <ankitchudasama108@gmail.com>
2023-10-20 10:53:05 +0530

    new project

---
+++ p2.js
@@ -0,0 +1,6 @@
+let name = "ankit";
+console.log(name);
+let fathername = "jentilal";
+console.log(fathername);
+let mothername = "harshaben";
+console.log(mothername);
---
+++ p4.js
@@ -0,0 +1,15 @@
+console.log("---------ｍａｓａｉｓｃｈｏｏｌ---------");
+console.log("---------Rᴇᴘᴏʀᴛ_ᴄᴀʀᴅ---------");
+console.log("------student_name-----")
+let name = "Ankit";
+console.log(name);
+console.log("-----section-----");
+let s = "cap_06";
+console.log(s);
+console.log("-----Roll_Number-----")
+let R = 45;
+console.log(R);
+console.log("-----Subject_and_Grads-----");
+console.log("1.HTML---A+");
+console.log("2.CSS---A+");
+console.log("3.JavaScript---B+");
